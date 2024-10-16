df5d82b41067a78cf791da4cb99520e68f3d4f22
Arsen Stepanyan <ar.step@icloud.com>
2024-10-16 14:12:34 +0200

    wrote some tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class TemplatesTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_solution_page_status_code(self):
+        response = self.client.get('/solutions/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_resources_page_status_code(self):
+        response = self.client.get('/resources/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_careers_page_status_code(self):
+        response = self.client.get('/careers/')
+        self.assertEqual(response.status_code, 200)
